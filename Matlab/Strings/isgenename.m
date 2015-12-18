4f856ad34673c3fc70d0008d548d11b826cf5671
Anastasia Baryshnikova <abarysh@princeton.edu>
2015-12-18 18:32:49 -0500

    Updated isgenename.m

--- Matlab/Strings/isgenename.m
+++ Matlab/Strings/isgenename.m
@@ -3,4 +3,4 @@ function bool = isgenename(vector)
 num = cellfun(@isnumeric, vector);
 
 bool = zeros(size(vector));
-bool(~num) = ~cellfun(@isempty, regexpi(vector(~num), '^[A-Z]{3,}[0-9]{1,4}[AB]$'));
+bool(~num) = ~cellfun(@isempty, regexpi(vector(~num), '^[A-Z]{3,}[0-9]{1,4}([AB])?$'));
