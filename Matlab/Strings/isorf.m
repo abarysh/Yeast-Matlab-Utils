57f610a1ec2941791790f7c1b3d44fe2eeb31d46
Anastasia Baryshnikova <abarysh@princeton.edu>
2015-12-18 22:54:07 -0500

    Updated isorf.m

--- Matlab/Strings/isorf.m
+++ Matlab/Strings/isorf.m
@@ -3,5 +3,5 @@ function bool = isorf(vector)
 num = cellfun(@isnumeric, vector);
 
 bool = zeros(size(vector));
-bool(~num) = ~cellfun(@isempty, regexpi(vector(~num), '^Y[A-P][RL][0-9]{3}[CW](-[A-E])*$'));
+bool(~num) = ~cellfun(@isempty, regexp(vector(~num), '^Y[A-P][RL][0-9]{3}[CW](-[A-E])*$'));
 bool = logical(bool);
