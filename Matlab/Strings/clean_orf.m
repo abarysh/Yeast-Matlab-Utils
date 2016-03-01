5e140c3b9ccff57d76bfa27234d91eb93134121a
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-03-01 17:35:45 -0500

    Enabled clean_orf to run on mixed cell arrays

--- Matlab/Strings/clean_orf.m
+++ Matlab/Strings/clean_orf.m
@@ -1,5 +1,8 @@
 % Remove all symbols that should not appear in an ORF name
 function newNames = clean_orf(oldNames)
 
-newNames = regexprep(oldNames, '[_ .,''"`!@#$%^&*()[]{}+=|~]','');
-newNames = upper(newNames);
+newNames = oldNames;
+
+inds = find(~cellfun(@isnumeric, oldNames) & ~cellfun(@isempty, oldNames));
+newNames(inds) = regexprep(oldNames(inds), '[_ .,''"`!@#$%^&*()[]{}+=|~]','');
+newNames(inds) = upper(newNames(inds));
