cf8d2e7830bc26d4b0ced1d2007eeeda8fe859f9
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-04-28 17:38:31 -0400

    Added a few checks and generalizations to write_matrix_file.m

--- Matlab/IO/write_matrix_file.m
+++ Matlab/IO/write_matrix_file.m
@@ -1,4 +1,4 @@
-function write_matrix_file(fid, labels_rows, labels_cols, data)
+function write_matrix_file(fid, labels_rows, labels_cols, data, varargin)
 
 % Inputs:
 %
@@ -8,20 +8,62 @@ function write_matrix_file(fid, labels_rows, labels_cols, data)
 % * |labels_row| = |p x q| cell array of labels, where |p| is the number of
 % rows and |q| is the number of labels for each row.
 
+%% Check inputs
+
+PROGRESSBAR = 0;
+if ~isempty(find(strcmpi('ProgressBar', varargin)))
+    PROGRESSBAR = varargin{find(strcmpi('ProgressBar', varargin))+1};
+end
+
+PROGRESSTITLE = 'Writing the matrix file...';
+if ~isempty(find(strcmpi('ProgressTitle', varargin)))
+    PROGRESSTITLE = varargin{find(strcmpi('ProgressTitle', varargin))+1};
+end
+
 str = '\t%.3f';
 if isinteger(data)
     str = '\t%d';
 end
 
-fprintf('Writing the matrix file\n');
+if ~iscell(labels_rows)
+    error('Row labels are supposed to be a cell array.');
+end
 
-fprintf(fid,'\\');
+if ~iscell(labels_cols)
+    error('Column labels are supposed to be a cell array.');
+end
+
+nrows = size(data,1);
+ncols = size(data,2);
+
+if size(labels_rows,1) ~= nrows
+    error('The number of row labels does not match the number of rows in the data matrix. For a %d x %d data matrix, row labels are expected to be a %d x m cell array (with m > 0).', nrows, ncols, nrows);
+end
+
+if size(labels_cols,1) ~= ncols
+    error('The number of column labels does not match the number of columns in the data matrix. For a %d x %d data matrix, column labels are expected to be a %d x p cell array (with p > 0).', nrows, ncols, ncols);
+end
+
+
+%%
+
+if PROGRESSBAR
+    w = waitbar(0, PROGRESSTITLE, 'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
+    setappdata(w, 'canceling', 0);
+end
+
+fprintf([PROGRESSTITLE '\n']);
+fprintf(['|', blanks(100), '|\n']);
+fprintf('|');
+
+
+fprintf(fid,'ORF');
 
 for n = 1 : size(labels_cols,2)
     if n > 1
         fprintf(fid, '\n');
     end
-    for m = 1 : size(labels_cols,1)
+    for m = 1 : ncols
         if m == 1
             fprintf(fid, repmat('\t', 1, size(labels_rows,2)-1));
         end
@@ -29,20 +71,26 @@ for n = 1 : size(labels_cols,2)
     end
 end
 
-fprintf(['|', blanks(100), '|\n']);
-fprintf('|');
-
 y = 0;
 
-
-for p = 1 : size(labels_rows,1)
+for p = 1 : nrows
+    
+    % Check for Cancel button press
+    if PROGRESSBAR && getappdata(w,'canceling')
+        delete(w);
+        error('Canceled.');
+    end
     
     % -- Progress bar ---
-    x = fix(p * 100 / size(labels_rows,1));
+    if PROGRESSBAR
+        waitbar(p/nrows, w);
+    end
+    
+    x = fix(p * 100 / nrows);
     if x > y
         fprintf('*');
         y = x;
-    end
+    end 
     % -- Progress bar ---
         
     for q = 1 : size(labels_rows,2)
@@ -53,8 +101,13 @@ for p = 1 : size(labels_rows,1)
         end
     end
     
-    for m = 1 : size(labels_cols,1)
+    for m = 1 : ncols
         fprintf(fid, str, data(p,m));
     end
 end
+
+if PROGRESSBAR
+    delete(w);
+end
+
 fprintf('|\n');
