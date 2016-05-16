3983e9d58905c5334c3ece72f365b6538b688242
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-05-16 15:16:32 -0400

    Updated included -F, -G, -H orfs

--- Matlab/Strings/is_orf.m
+++ Matlab/Strings/is_orf.m
@@ -3,5 +3,5 @@ function bool = is_orf(vector)
 num = cellfun(@isnumeric, vector);
 
 bool = zeros(size(vector));
-bool(~num) = ~cellfun(@isempty, regexp(vector(~num), '^Y[A-P][RL][0-9]{3}[CW](-[A-E])*$'));
+bool(~num) = ~cellfun(@isempty, regexp(vector(~num), '^Y[A-P][RL][0-9]{3}[CW](-[A-H])*$'));
 bool = logical(bool);
