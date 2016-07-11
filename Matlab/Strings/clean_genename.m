09d028302696b900d326bded7689f8cfc40353c9
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-07-11 16:26:39 -0400

    Clean_genenames now more flexible, can deal with empty and numeric entries

--- Matlab/Strings/clean_genename.m
+++ Matlab/Strings/clean_genename.m
@@ -1,4 +1,7 @@
 % Remove all symbols that should not appear in a gene name
 function newNames = clean_genename(oldNames)
 
-newNames = regexprep(oldNames, '[ .]','');
+newNames = oldNames;
+inds = find(~cellfun(@isnumeric, oldNames) & ~cellfun(@isempty, oldNames));
+
+newNames(inds) = regexprep(oldNames(inds), '[ .]','');
