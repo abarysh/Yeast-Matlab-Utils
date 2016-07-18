85b5dbeb34ed86ffdcc90e0e7bb9aadf74ca3494
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-07-18 11:22:57 -0400

    Added correction for missing hyphens to clean_orf.m

--- Matlab/Strings/clean_orf.m
+++ Matlab/Strings/clean_orf.m
@@ -7,3 +7,14 @@ inds = find(~cellfun(@isnumeric, oldNames) & ~cellfun(@isempty, oldNames));
 newNames(inds) = regexprep(oldNames(inds), '[_ .,''"`!@#$%^&*()[]{}+=|~]','');
 newNames(inds) = upper(newNames(inds));
 newNames(inds) = strtrim(newNames(inds));
+
+%% Correct missing hyphens (e.g., YAL064CA)
+
+orf_length = cellfun(@length, newNames);
+orf_lastchar = ~cellfun(@isempty, regexp(newNames, '[ABCD]$'));
+
+inds = find(orf_length==8 & orf_lastchar);
+
+for i = 1 : length(inds)
+    newNames{inds(i)} = [newNames{inds(i)}(1:7) '-' newNames{inds(i)}(8)];
+end
