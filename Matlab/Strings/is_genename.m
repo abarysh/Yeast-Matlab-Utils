6c839bc1a250ef1b26c6f6a3e4e253302ae1b221
Anastasia Baryshnikova <abarysh@princeton.edu>
2017-03-23 11:30:33 -0700

    Updated uncharacterized, verified & dubious. Updated is_genename.m

--- Matlab/Strings/is_genename.m
+++ Matlab/Strings/is_genename.m
@@ -5,18 +5,16 @@ num = cellfun(@isnumeric, vector);
 bool = zeros(size(vector));
 bool(~num) = ~cellfun(@isempty, regexpi(vector(~num), '^[A-Z]{3,}[0-9]{1,4}([A-E])?$'));
 
-bool(~cellfun(@isempty, regexpi(vector(~num), '^RP[LS]?[0-9]+[A-E]*'))) = 1;
-bool(~cellfun(@isempty, regexpi(vector(~num), '^[A-Z]{3,}[0-9]{1,4}-[0-9]$'))) = 1;
+bool(~cellfun(@isempty, regexpi(vector(~num), '^RP[LS]?[0-9]+[A-E]*'))) = 1;    % example: RPL45A
+bool(~cellfun(@isempty, regexpi(vector(~num), '^[A-Z]{3,}[0-9]{1,4}-[0-9]$'))) = 1; % example: YRF1-6
+bool(~cellfun(@isempty, regexpi(vector(~num), '^[A-Z]{3,}[0-9],[0-9]$'))) = 1;  % example: ADE5,7
+bool(~cellfun(@isempty, regexpi(vector(~num), '^OM[0-9]{2}$'))) = 1;    % example: OM45
 
 % Exceptions to the rule
 bool(strcmp('HO', vector)) = 1;
-bool(strcmp('OM45', vector)) = 1;
-bool(strcmp('YRF1-6', vector)) = 1;
-bool(strcmp('ADE5,7', vector)) = 1;
-bool(strcmp('ARG5,6', vector)) = 1;
 bool(strcmp('MF(ALPHA)1', vector)) = 1;
 bool(strcmp('MF(ALPHA)2', vector)) = 1;
-bool(strcmp('DUR1,2', vector)) = 1;
+bool(strcmp('IMP2''', vector)) = 1;
 
 
 
